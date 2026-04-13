6d9ca054cfbfec8af4d8b35ac5a56833e68c4048
araujoestelaine-hue <araujoestelaine@gmail.com>
2026-04-13 20:49:16 -0300

    adicionando interação

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function mudar_conteudo_elemento(){
   document.getElementById("p").innerHTML = "Minha função de mudar o conteúdo funcionou!";
 
 }
+
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
